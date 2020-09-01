cf07a46308b3e2aa7e46af17fb31e2ff2a514bc0
Ron Wynn <rcwynn@gmail.com>
2020-08-31 20:37:42 -0400

    WIP. Adding Model, Controller, Repo

---
+++ backend-api/RopeFinalProjectBackEnd/Controllers/QuestionController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RopeFinalProjectBackEnd.Models;
+using RopeFinalProjectBackEnd.Repositories;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RopeFinalProjectBackEnd.Controllers
+{
+   [Route("api/[controller]")]
+   [ApiController]
+   public class QuestionController : ControllerBase
+   {
+      private IRepository<Question> questionRepo;
+
+      public QuestionController(IRepository<Question> otherRepo)
+      {
+         this.questionRepo = otherRepo;
+      }
+
+      public IEnumerable<Question> Get()
+      {
+         return questionRepo.GetAll();
+      }
+
+      // GET api/<QuestionController>/5
+      [HttpGet("{id}")]
+      public Question Get(int id)
+      {
+         return questionRepo.GetById(id);
+      }
+
+      // POST api/<QuestionController>
+      [HttpPost]
+      public IEnumerable<Question> Post([FromBody] Question value)
+      {
+         questionRepo.Create(value);
+         return questionRepo.GetAll();
+      }
+
+      // PUT api/<QuestionController>/5
+      [HttpPut("{id}")]
+      public void Put(int id, [FromBody] string value)
+      {
+      }
+
+      // DELETE api/<QuestionController>/5
+      [HttpDelete("{id}")]
+      public void Delete(int id)
+      {
+      }
+   }
+}
---
+++ backend-api/RopeFinalProjectBackEnd/Models/Question.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RopeFinalProjectBackEnd.Models
+{
+   public class Question
+   {
+      public int ID { get; set; }
+      public string QuestionText { get; set; }
+
+   }
+}
